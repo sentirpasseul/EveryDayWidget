82e94fa1325ae62801937b332161aff0c3b4f755
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2022-09-01 19:23:52 +0800

    Правка по отступам элементов виджета

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -44,29 +44,21 @@ class MainUi(QWidget):
 
     def update_time(self):
         today = datetime.datetime.now()
-        self.time = (today.strftime("%H:%M:%S"))
+        self.time = (today.strftime("%H:%M"))
 
         self.label_time.setText(self.time)
         #self.label_time = QLabel(f'{self.time}')  # Текст для отображения времени
         self.label_time.setFont(PyQt5.QtGui.QFont('Inter', 24))  # Изменение шрифта отображения времени
-        self.label_time.setStyleSheet('color: white')
+        self.label_time.setStyleSheet('color: white; ')
 
-        self.label_date.setText(self.full_day) # Текст для отображения даты
+        self.label_date.setText(self.full_day)  # Текст для отображения даты
         self.label_date.setFont(PyQt5.QtGui.QFont('Inter', 16))  # Изменение шрифта отображения даты
-        self.label_date.setStyleSheet('color: white; padding-left: 30px; padding-down: 50px')
-
-
+        self.label_date.setStyleSheet('color: white; padding-left: 10px; padding-down: 50px')
 
     def update_weather(self):
         self.label_degree.setText(self.weather_degree)  # Текст для отображения
         self.label_degree.setFont(PyQt5.QtGui.QFont('Inter', 36))  # Изменение шрифта отображения градусов
         self.label_degree.setStyleSheet('color: white')
-
-    def get_weather(self):
-        ...
-
-
-
 
 
     def initUI(self):
@@ -77,8 +69,10 @@ class MainUi(QWidget):
         self.timer_time.timeout.connect(self.update_time)
 
         self.timer_weather = QTimer()
-        self.timer_weather.start(1800000)
+        self.timer_weather.start(600000)
         self.timer_weather.timeout.connect(self.update_weather)
+
+
 
 
 
@@ -87,9 +81,9 @@ class MainUi(QWidget):
         #label_weather.setText('f')
         label_icon_weather = QtWidgets.QLabel()              #Инициализация корпуса погоды
         pixmap_icon_weather = QPixmap(self.path_icon_weather) #Иконка погоды с указанием пути к ней
-        #pixmap_icon_weather.scaled(90, 75) #Масштабирование иконки погоды
+        pixmap_icon_weather.scaled(90, 90) #Масштабирование иконки погоды
         label_icon_weather.setPixmap(pixmap_icon_weather) #Добавление иконки в корпус Label
-        label_icon_weather.setStyleSheet('color: white; opacity: 0.72; padding-up: 50px; paddding-left: 5px')
+        label_icon_weather.setStyleSheet('color: white; opacity: 0.72; margin-left: 10px')
 
         label_city = QtWidgets.QLabel(f'{self.city}') #Текст для отображения города
         label_city.setFont(PyQt5.QtGui.QFont('Inter', 36)) #Изменение шрифта отображения города
