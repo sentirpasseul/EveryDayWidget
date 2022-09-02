1f84800ef19abd33a26a19b4f43beaaa7fb456ed
sentirpasseul <71366294+sentirpasseul@users.noreply.github.com>
2022-09-02 09:09:31 +0800

    Добавлены условия для изменения погодных иконок

--- parsers/weatherIcon.py
+++ parsers/weatherIcon.py
@@ -21,6 +21,7 @@ class WeatherIcon:
         self.sun_clouds = QPixmap(r'..\src\icons\weather\day\cloud_sun.png')
         self.clouds = QPixmap(r'..\src\icons\weather\day\clouds.png')
         self.fog = QPixmap(r'..\src\icons\weather\day\fog.png')
+        self.drizzle = QPixmap(r'..\src\icons\weather\day\drizzle.png')
         self.rain = QPixmap(r'..\src\icons\weather\day\rain.png')
         self.snow = QPixmap(r'..\src\icons\weather\day\snow.png')
         self.thunder = QPixmap(r'..\src\icons\weather\day\thunder.png')
@@ -30,12 +31,34 @@ class WeatherIcon:
     def get_weather(self):
         icon_w = self.weather_code
         #print('ICON_W', icon_w)
+        sun = [0, 1]
+        fog = [item for item in range(45, 49)]
+        drizzle = [item for item in range(51, 58)]
+        rain = [item for item in range(61, 68)]
+        snow = [item for item in range(71, 78)]
+        rain_showers = [item for item in range(80, 87)]
+        snow_showers = [85, 86]
+        thunder = ['95 *', 96, '99 *']
 
-        #if icon_w == 'Clouds' or ('clouds' in icon_w):
-        #    return self.rain
-        #elif icon_w == 'Rain' or ('rain' in icon_w):
-        #    return self.rain
-        #elif icon_w == 'Sunny' or ('sunny' in icon_w):
-        return self.clouds
+        if icon_w in sun:
+            return self.sun
+        elif icon_w == 2:
+            return self.sun_clouds
+        elif icon_w == 3:
+            return self.clouds
+        elif icon_w in fog:
+            return self.fog
+        elif icon_w in drizzle:
+            return self.drizzle
+        elif icon_w in rain:
+            return self.rain
+        elif icon_w in snow:
+            return self.snow
+        elif icon_w in rain_showers:
+            return self.rain            #!!TODO Сделать для ливней отдельную иконку
+        elif icon_w in snow_showers:
+            return self.snow            #!!TODO Сделать для снежных ливней отдельную иконку
+        elif icon_w in thunder:
+            return self.thunder
 
 
